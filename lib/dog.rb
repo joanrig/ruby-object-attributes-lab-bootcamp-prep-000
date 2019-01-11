7eea85e270fc5844450106706254198db467cef1
Joan in DC <joanrigdon@gmail.com>
2019-01-11 19:30:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,10 @@
 class Dog 
-  def name 
-    def name=(name)
-      @name = name
-    end
+  def name=(name)
+    @name = name
+  end
     
-    def name
-      "{@name}"
-    end
+  def name
+    "{@name}"
+  end
+end
+  
